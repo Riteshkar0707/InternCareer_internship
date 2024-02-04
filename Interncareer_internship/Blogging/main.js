0b5039ca78fbb6e39ac6fe1ebde1909d73257a27
Riteshkar0707 <146061235+Riteshkar0707@users.noreply.github.com>
2024-02-04 21:24:19 +0530

    Add files via upload

---
+++ Interncareer_internship/Blogging/main.js
@@ -0,0 +1,14 @@
+function login() {
+    // Add logic for handling login
+    console.log("Login functionality will be implemented here.");
+  }
+  
+  function register() {
+    // Add logic for handling registration
+    console.log("Registration functionality will be implemented here.");
+  }
+  
+  function savePost() {
+    // Add logic for saving a blog post
+    console.log("Blog post saving functionality will be implemented here.");
+  }
